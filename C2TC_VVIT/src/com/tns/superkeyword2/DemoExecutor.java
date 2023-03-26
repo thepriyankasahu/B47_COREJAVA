b33bcbcaf9018a65b5ff688f265751c758bacf99
sahur <sahur@DESKTOP-NF042P3>
2023-03-26 21:34:03 +0530

    first commit

---
+++ C2TC_VVIT/src/com/tns/superkeyword2/DemoExecutor.java
@@ -0,0 +1,13 @@
+package com.tns.superkeyword2;
+
+public class DemoExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		DemoChild d=new DemoChild();
+		  d.display();
+		  d.test();
+
+	}
+
+}
