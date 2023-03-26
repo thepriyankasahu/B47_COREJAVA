94b4d137bfd1bce7529db170295765ef5e4cc020
sahur <sahur@DESKTOP-NF042P3>
2023-03-26 21:53:12 +0530

    first commit

---
+++ C2TC_VVIT/src/com/tns/overloading/OverloadingExecutor.java
@@ -0,0 +1,18 @@
+package com.tns.overloading;
+
+public class OverloadingExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Point ob=new Point();   //default constructor is called
+		 System.out.println(ob);
+		 
+		 Point ob1=new Point(14.5f);
+		 System.out.println(ob1);
+		 
+		 Point ob2=new Point(14.5f,2.5f);
+		 System.out.println(ob2);
+
+	}
+
+}
