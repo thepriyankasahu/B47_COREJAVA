3ecae552f415e862f88ccec51fb55b5d96ff5d12
sahur <sahur@DESKTOP-NF042P3>
2023-03-26 21:18:06 +0530

    first commit

---
+++ C2TC_VVIT/src/com/tns/packagetwo/SubClass.java
@@ -0,0 +1,17 @@
+package com.tns.packagetwo;
+
+import com.tns.packageone.Base;
+
+public class SubClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Base b=new Base();
+		b.methodPublic();
+		//b.methodDefault();
+		//b.methodProtected();
+		//b.methodPrivate();	
+
+	}
+
+}
