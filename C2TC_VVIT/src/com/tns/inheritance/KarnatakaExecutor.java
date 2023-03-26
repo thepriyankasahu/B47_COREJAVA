9cba716fdebdea271e2939306c4b26b7bedd02f6
sahur <sahur@DESKTOP-NF042P3>
2023-03-26 21:38:56 +0530

    first commit

---
+++ C2TC_VVIT/src/com/tns/inheritance/KarnatakaExecutor.java
@@ -0,0 +1,15 @@
+package com.tns.inheritance;
+
+public class KarnatakaExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Karnataka k=new Karnataka();
+		Kerala kk=new Kerala();
+		
+		k.displayLocalKarnataka();
+		k.displayNational();
+		kk.displayLocalKerala();
+	}
+
+}
