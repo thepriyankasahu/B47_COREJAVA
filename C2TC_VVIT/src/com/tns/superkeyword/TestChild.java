bd242824299fdcee015ac3506103ef3fd10eee23
sahur <sahur@DESKTOP-NF042P3>
2023-03-26 21:23:43 +0530

    first commit

---
+++ C2TC_VVIT/src/com/tns/superkeyword/TestChild.java
@@ -0,0 +1,24 @@
+package com.tns.superkeyword;
+
+class TestParent
+{
+	protected void test()
+	{
+		System.out.println("This is a parent class");
+	}
+}
+public class TestChild extends TestParent
+{
+  protected void test()
+  {
+	  System.out.println("This is a child calss test");
+  }
+  void display()
+  {
+	  //test();
+	  super.test();
+  }
+}
+
+
+
---
+++ C2TC_VVIT/src/com/tns/superkeyword/TestExecutor.java
@@ -0,0 +1,13 @@
+package com.tns.superkeyword;
+
+public class TestExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		TestChild c=new TestChild();
+		c.display();
+		c.test();
+
+	}
+
+}
