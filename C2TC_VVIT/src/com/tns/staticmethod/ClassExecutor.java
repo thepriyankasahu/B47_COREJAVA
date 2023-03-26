984e768fb783e5ce152d4cb1ef9f9c531269cedf
sahur <sahur@DESKTOP-NF042P3>
2023-03-26 22:04:33 +0530

    first commit

---
+++ C2TC_VVIT/src/com/tns/staticmethod/ClassExecutor.java
@@ -0,0 +1,13 @@
+package com.tns.staticmethod;
+
+public class ClassExecutor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Myclass.display();
+		 Myclass o=new Myclass();   
+		 System.out.println(o);
+
+	}
+
+}
